bc62ec8dcc152b60cf645834dfbc3a1c8455b934
Marc Marcos <marcmarcosmadruga@gmail.com>
2023-11-18 21:19:06 +0100

    structure changed

--- 2022/Day 4/index.py
+++ 2022/Day 4/index.py
@@ -6,17 +6,17 @@ with open('input_2.txt', 'r') as f:
         a, b = line.split(",") 
         b = b.rstrip()
 
+        print(f"{a} y {b}.")
+
         a1, a2 = a.split("-")
         b1, b2 = b.split("-")
 
         if ((int(a2) - int(a1)) >= (int(b2) - int(b1))):
             if (a1 <= b1 and a2 >= b2): 
-                print(f"{a1}-{a2}:{b1}-{b2}")
                 suma += 1
 
         else:
             if (b1 <= a1 and b2 >= a2): 
-                print(f"{a1}-{a2}:{b1}-{b2}")   
                 suma += 1
         
 
